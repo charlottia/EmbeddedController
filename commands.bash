4b4f4194d161853040efe987115a733acb64d912
Charlotte <charlotte@lottia.net>
2024-12-05 21:45:25 +1100

    move all pypi installs to nix shell in prep for derivation.

--- commands.bash
+++ commands.bash
@@ -5,10 +5,12 @@ set -euxo pipefail
 git submodule init
 git submodule update
 
+# reset
+git submodule foreach 'git clean -ffdx; git checkout .'
+rm -rf venv
+
 python3 -m venv venv
 source venv/bin/activate
-pip install -U pip
-pip install pyyaml pykwalify packaging pyelftools colorama setuptools==68.2.2
 
 sed -e 's#"/bin:/usr/bin"#"/bin:/usr/bin:'"$(dirname "$(which gcc)"):$(dirname "$(which dtc)")"'","DYLD_LIBRARY_PATH":"'"$(dirname "$(dirname "$(which dtc)")")/lib"'"#'  -i src/platform/ec/zephyr/zmake/zmake/jobserver.py
 pip install src/platform/ec/zephyr/zmake
--- flake.nix
+++ flake.nix
@@ -16,14 +16,23 @@
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {inherit system;};
+
+      python = pkgs.python312;
+      pythonPkgs = pkgs.python312Packages;
     in {
       formatter = pkgs.alejandra;
 
       devShells.default = pkgs.mkShell {
         buildInputs = with pkgs; [
           (zephyr-nix.packages.${system}.sdk-0_16.override {targets = ["arm-zephyr-eabi"];})
-          python312
-          python312Packages.libfdt  # <- 311 version fails to build
+          python
+          pythonPkgs.pyyaml
+          pythonPkgs.pykwalify
+          pythonPkgs.packaging
+          pythonPkgs.pyelftools
+          pythonPkgs.colorama
+          pythonPkgs.setuptools
+          pythonPkgs.libfdt  # <- 311 version fails to build; can't build with pip.
           dtc
           cmake
           ninja
