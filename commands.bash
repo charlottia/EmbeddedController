f082b36b800cb91b02710a8c747387ecbb3c8910
Charlotte <charlotte@lottia.net>
2024-12-05 21:02:37 +1100

    init.

---
+++ commands.bash
@@ -0,0 +1,18 @@
+python3 -m venv venv
+source venv/bin/activate
+pip install -U pip
+pip install pyyaml pykwalify packaging pyelftools colorama setuptools==68.2.2
+
+sed -e 's_"/bin:/usr/bin"_"/bin:/usr/bin:'"$(dirname "$(which gcc)"):$(dirname "$(which dtc)")"'"_'  -i src/platform/ec/zephyr/zmake/zmake/jobserver.py
+pip install src/platform/ec/zephyr/zmake
+
+sed -e 's/>=61.0/==68.2.2/' -i src/third_party/u-boot/files/tools/dtoc/pyproject.toml
+sed -e 's/"pylibfdt"/"libfdt"/' -i src/third_party/u-boot/files/tools/dtoc/pyproject.toml
+pip install src/third_party/u-boot/files/tools/dtoc
+
+sed -e 's/>=61.0/==68.2.2/' -i src/third_party/u-boot/files/tools/binman/pyproject.toml
+sed -e 's/"pylibfdt"/"libfdt"/' -i src/third_party/u-boot/files/tools/binman/pyproject.toml
+pip install src/third_party/u-boot/files/tools/binman
+
+# WIP:
+zmake -j8 build lotus --clobber -DCMAKE_MAKE_PROGRAM=(which ninja) -DGIT_EXECUTABLE=(which git)
---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Framework Laptop Embedded Controller (EC) build environment";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+
+    zephyr-nix.url = github:adisbladis/zephyr-nix;
+    zephyr-nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    zephyr-nix,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      formatter = pkgs.alejandra;
+
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          # pkgsCross.arm-embedded.buildPackages.gcc
+          (zephyr-nix.packages.${system}.sdk-0_16.override {targets = ["arm-zephyr-eabi"];})
+          gcc-arm-embedded
+          gcc
+          gnumake
+          git-repo
+          swig # or swig3?
+          python312
+          python312Packages.libfdt  # <- 311 version fails to build
+          dtc
+          cmake
+          file
+          ninja
+          wget
+          uv
+        ];
+      };
+    });
+}
